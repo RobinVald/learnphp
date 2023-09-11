df62ffaa0c3919896e81d7b142271a084a44874f
Robin Valdmets <robin.valdmets@gmail.com>
2023-09-11 13:15:20 +0300

    add easy server script

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
-var_dump($_SERVER);
+//var_dump($_SERVER);
+switch($_SERVER['REQUEST_URI']) {
+    case'/':
+        echo 'home page';
+        break;
+    case'/about':
+        echo 'about';
+        break;
+    default:
+        echo '404 error, hehehehe get shafted';
+        break;
+}
