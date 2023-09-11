a17201d88578daa866a9f8a8a5a8255b1ccdc75b
Robin Valdmets <robin.valdmets@gmail.com>
2023-09-11 15:08:37 +0300

    add PublicControllers

--- public/index.php
+++ public/index.php
@@ -1,14 +1,42 @@
 <?php
 
-//var_dump($_SERVER);
-switch($_SERVER['REQUEST_URI']) {
-    case'/':
-        echo 'home page';
-        break;
-    case'/about':
-        echo 'about';
-        break;
-    default:
-        echo '404 error, hehehehe get shafted';
-        break;
+spl_autoload_register(function($className){
+    //var_dump($className);
+    $className = substr($className,4);
+    require_once __DIR__ . "/../src/$className.php";
+});
+
+use App\Router;
+
+require __DIR__ . '/../routes.php';
+
+//var_dump(Router::$routes);
+
+$router = new Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if($match){
+    if(is_callable($match['action'])){
+        call_user_func($match['action']);
+    } else if(is_array($match['action'])){
+        $class = $match['action'][0];
+        $controller = new $class();
+        $method = $match['action'][1];
+        $controller->$method();
+    }
+} else{
+    echo '404 shafted';
 }
+
+
+//var_dump($_SERVER);
+//switch($_SERVER['REQUEST_URI']) {
+//   case'/':
+//      echo 'home page';
+//        break;
+//    case'/about':
+//        echo 'about';
+//        break;
+//    default:
+//        echo '404 error, hehehehe get shafted';
+//        break;
+//}
