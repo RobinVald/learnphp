18a3b0693552289fe1486140a410673420727663
Robin Valdmets <robin.valdmets@gmail.com>
2023-09-11 13:03:29 +0300

    new file

---
+++ views/home.php
@@ -0,0 +1,8 @@
+<?php include 'partials/header.php';?>
+<?php $num = 10; ?> 
+<?php if($num > 10): ?>
+        <h1>suurem</h1>
+    <?php else: ?>
+            <h1>Vaiksem</h1>
+            <?php endif?>
+<?php include 'partials/footer.php';?>
